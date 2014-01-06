b8a2152f0777920f8011e83e08cc11985593d793
Danny Arnold <despairblue@skeil.de>
2014-01-06 02:04:00 +0100

    Rework api

--- app/scripts/services/project.js
+++ app/scripts/services/project.js
@@ -3,7 +3,7 @@
 angular.module('scegratooApp')
 .service('Project', function Project($resource) {
   // AngularJS will instantiate a singleton by calling "new" on this function
-  return $resource('api/v1/projects.json', {}, {
-    query: {method:'GET', params:{}, isArray:true}
+  return $resource('api/v1/project/:projectId.json', {}, {
+    query: {method:'GET', params:{projectId: 'projects'}, isArray:true}
   })
 })
