2ec488483169a37e5f0fc8809f35970abea89854
Danny Arnold <despairblue@skeil.de>
2014-01-17 11:57:36 +0100

    Use karma-coverage to generate test coverage reports.

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,17 @@ module.exports = function(config) {
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
 
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/scripts/**/*.js': ['coverage'],
+      '**/*.html': ['ng-html2js']
+    },
+
     // list of files / patterns to load in the browser
     files: [
       'app/bower_components/angular/angular.js',
