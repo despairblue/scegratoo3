a436e70878eaf07963dae441244817c0ca025a99
Danny Arnold <despairblue@skeil.de>
2014-01-17 11:58:13 +0100

    Remove redundant file inclusion

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,6 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'app/scripts/*.js',
       'app/scripts/**/*.js',
       'app/views/**/*.html',
       'test/mock/**/*.js',
