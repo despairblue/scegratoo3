846265e48e76e1773fe3a7a3e597f6e8b7d0e407
Danny Arnold <despairblue@skeil.de>
2014-01-20 03:20:27 +0100

    Implement x3d file view

--- app/scripts/controllers/projects/:project/x3d/:file.js
+++ app/scripts/controllers/projects/:project/x3d/:file.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('scegratooApp')
-  .controller('ProjectsProjectX3dFileCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    window.x3dom.reload()
+  .controller('ProjectsProjectX3dFileCtrl', function ($scope, $routeParams, Project) {
+    Project.get({project: $routeParams.project, file: $routeParams.file}, function(file) {
+      $scope.x3d = file.data
+    })
+
+    // x3d.get().then(function(element) {
+    //   element.querySelector('inline').forEach(function(inline) {
+    //     $scope.transformations.push(encapsulate(inline))
+    //     inline.addEventListener('click', function(i) {
+    //       $scope.currentElement = i
+    //     })
+    //   })
+    // })
+
+    // function encapsulate(inline) {
+    //   console.log(inline)
+    // }
   });
--- app/scripts/services/project.js
+++ app/scripts/services/project.js
@@ -1,10 +1,39 @@
 'use strict';
 
 angular.module('scegratooApp')
-.service('Project', function Project($resource) {
+.service('Project', function Project($resource, $http) {
   // AngularJS will instantiate a singleton by calling "new" on this function
-  var apiRoot = 'api/v1/projects/'
-  return $resource(apiRoot + ':project.json', {}, {
-    query: {method:'GET', params:{project: 'projects'}, isArray:true}
-  })
+  var apiRoot = 'api/v1'
+  var route = apiRoot + '/projects/:project.:format'
+  var resource = $resource(route, {format: 'json'})
+  return {
+    get: function(params, fn) {
+      if (params.file) {
+        var url = [
+          apiRoot,
+          'projects',
+          encodeURI(params.project),
+          encodeURI(params.file)
+        ].join('/')
+        var res = $resource(url, {}, {
+          get: {method: 'GET', transformResponse: function(data) {
+            return {data: data}
+          }}
+        })
+        return res.get(fn)
+      } else {
+        return resource.get(params, fn)
+      }
+    },
+    query: resource.query
+    // getFile: function(params) {
+    //   var url = [
+    //     apiRoot,
+    //     'projects',
+    //     encodeURI(params.project),
+    //     encodeURI(params.file)
+    //   ].join('/')
+    //   return $http.get(url)
+    // }
+  }
 })
