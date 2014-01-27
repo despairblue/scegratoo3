3368e0b2735f1657cc078f6e7513cc4bc91ff0bf
Danny Arnold <despairblue@skeil.de>
2014-01-27 23:31:09 +0100

    Translate inline urls to api compliant urls

--- test/spec/directives/sgt-x3d.js
+++ test/spec/directives/sgt-x3d.js
@@ -7,8 +7,11 @@ describe('Directive: sgtX3d', function() {
 
   var element, scope;
 
-  beforeEach(inject(function($rootScope, $compile, $window) {
+  beforeEach(inject(function($rootScope, $compile, $window, $routeParams) {
     scope = $rootScope.$new();
+    // set up routes
+    $routeParams.project = 'OrgelRT'
+    $routeParams.file = 'src/orgelSS13.x3d'
     // set up element, compile and link it
     element = angular.element('<div sgt-x3d content="x3d"></div>')
     element = $compile(element)(scope)
@@ -43,4 +46,11 @@ describe('Directive: sgtX3d', function() {
     scope.$digest()
     expect($window.x3dom.reload.calls.length).toBe(2)
   }))
+
+  it('should translate the inline\'s url', function() {
+    scope.x3d = '<Inline url="x3d/awesomeModel.x3d"></Inline>'
+    scope.$digest()
+    var e = element[0].querySelector('inline')
+    expect(e.getAttribute('url')).toBe('api/v1/projects/OrgelRT/src/x3d/awesomeModel.x3d')
+  })
 });
