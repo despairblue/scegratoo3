564c4b362443e52b748d332bc9083e2166ee27db
Danny Arnold <despairblue@skeil.de>
2014-01-27 23:31:09 +0100

    Translate inline urls to api compliant urls

--- app/scripts/directives/sgt-x3d.js
+++ app/scripts/directives/sgt-x3d.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('scegratooApp')
-  .directive('sgtX3d', function ($window) {
+  .directive('sgtX3d', function ($window, $routeParams, Constants) {
     return {
       // template: '',
       restrict: 'AE',
@@ -12,6 +12,17 @@ angular.module('scegratooApp')
         scope.$watch('content', function(content) {
           element.html(content)
           element.children().addClass('fullpage')
+
+          var inlines = element.find('inline')
+          angular.forEach(inlines, function(inline) {
+            var url = inline.getAttribute('url')
+            inline.setAttribute('url', Constants.apiRoot +
+              '/' + 'projects' +
+              '/' + $routeParams.project +
+              '/' + $routeParams.file.replace(/\/[^\/]*$/, '') +
+              '/' + url)
+          })
+
           $window.x3dom.reload()
         })
       }
