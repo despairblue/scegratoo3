5583cb3cdf5b786ac4ec39d898d0207f90c516fd
Danny Arnold <despair.blue@gmail.com>
2015-05-02 18:51:04 +0200

    clean up sgt-x3d

--- app/scripts/directives/sgt-x3d.js
+++ app/scripts/directives/sgt-x3d.js
@@ -4,7 +4,7 @@ const angular = window.angular
 const $ = window.$
 
 angular.module('scegratooApp')
-  .directive('sgtX3d', function SgtX3d ($window, $routeParams, Constants, X3domUtils, $http, $q, $templateCache) {
+  .directive('sgtX3d', function SgtX3d ($window, X3domUtils, $http, $q, $templateCache) {
     return {
       restrict: 'AE',
       link: (scope, element, attrs) => {
@@ -14,43 +14,41 @@ angular.module('scegratooApp')
           'templates/planeSensor-X.html',
           'templates/planeSensor-Y.html'
         ]
-        var promises = []
+        var promises = templates.map(value => $http.get(value, {cache: $templateCache}))
 
-        angular.forEach(templates, value => {
-          promises.push($http.get(value, {cache: $templateCache}))
-        })
-
-        $q.all(promises).then(results => {
-          // Normally this would be enough, but we want to make sure that the
-          // cached value is only the template and not the whole response object
-          // with status code, header, etc
-          angular.forEach(results, value => {
-            $templateCache.put(value.config.url, value.data)
+        $q.all(promises)
+          .then(results => {
+            // Normally this would be enough, but we want to make sure that the
+            // cached value is only the template and not the whole response object
+            // with status code, header, etc
+            results.forEach(value => {
+              $templateCache.put(value.config.url, value.data)
+            })
           })
-        }).then(() => {
-          // extract in directive
-          var gui = new $window.dat.GUI({autoPlace: false})
-          console.debug('Created ', gui)
-          element.parent().prepend($(gui.domElement)
-          .css('float', 'left')
-          .css('position', 'absolute')
-          .css('z-index', 1))
-          var guiCoordinates = gui.addFolder('Coordinates')
-          var guiSwitches = gui.addFolder('Switches')
+          .then(() => {
+            // extract in directive
+            var gui = new $window.dat.GUI({autoPlace: false})
+            console.debug('Created ', gui)
+            element.parent().prepend($(gui.domElement)
+            .css('float', 'left')
+            .css('position', 'absolute')
+            .css('z-index', 1))
+            var guiCoordinates = gui.addFolder('Coordinates')
+            var guiSwitches = gui.addFolder('Switches')
 
-          var options = X3domUtils.setUp(element)
+            var options = X3domUtils.setUp(element)
 
-          guiCoordinates.add(options, 'x').listen()
-          guiCoordinates.add(options, 'y').listen()
-          guiCoordinates.add(options, 'z').listen()
-          guiSwitches.add(options, 'useHitPnt')
-          guiSwitches.add(options, 'snapToGrid')
+            guiCoordinates.add(options, 'x').listen()
+            guiCoordinates.add(options, 'y').listen()
+            guiCoordinates.add(options, 'z').listen()
+            guiSwitches.add(options, 'useHitPnt')
+            guiSwitches.add(options, 'snapToGrid')
 
-          scope.$watch(attrs.content, content => {
-            element.html(content)
-            options = X3domUtils.setUp(element)
+            scope.$watch(attrs.content, content => {
+              element.html(content)
+              options = X3domUtils.setUp(element)
+            })
           })
-        })
       }
     }
   })
