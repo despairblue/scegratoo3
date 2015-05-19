b5b236e8a16399472a4f36c402694d52d7e7b43c
Danny Arnold <despair.blue@gmail.com>
2015-05-20 00:22:51 +0200

    greatly simplify rotation and translation imputs

--- app/scripts/services/treenodeattribute.js
+++ app/scripts/services/treenodeattribute.js
@@ -1,15 +1,13 @@
 'use strict'
 
 window.angular.module('scegratooApp')
-  .service('TreeNodeAttribute', (React, R) => {
+  .service('TreeNodeAttribute', (React, R, TreeNodeAttributeTextbox) => {
     const {
       contains,
-      curry,
       flatten,
       map,
       pipe,
       split,
-      take
     } = R
 
     const splitVector = pipe(
@@ -18,83 +16,40 @@ window.angular.module('scegratooApp')
       flatten
     )
 
-    const deserializeVector = pipe(
-      splitVector,
-      map(Number.parseFloat),
-      map(n => n.toFixed(2))
-    )
-
     return React.createClass({
       displayName: 'TreeNodeAttribute',
       propTypes: {
         owner: React.PropTypes.object.isRequired,
         attribute: React.PropTypes.object.isRequired
       },
-      getInitialState: function () {
-        return {
-          renderAttribute: true
-        }
-      },
-      mapAttributeToHandler: function (name, value, index) {
-        if (name === 'translation') {
-          return curry(this.translationChanged)(value, index)
-        } else if (name === 'rotation') {
-          return curry(this.rotationChanged)(value, index)
-        } else {
-          throw new Error('Unknown attribute')
-        }
-      },
-      clicked: function (event) {
+      changeHandler: function (event) {
         if (event.currentTarget.checked) {
           this.props.owner.render = true
         } else {
           this.props.owner.render = false
         }
       },
-      focus: function () {
-        this.setState({renderAttribute: false})
-      },
-      blur: function () {
-        this.setState({renderAttribute: true})
-      },
-      translationChanged: function (oldValue, index, event) {
-        const oldVector = deserializeVector(oldValue)
-        oldVector[index] = event.currentTarget.value
-        const newVector = take(3, oldVector).join(' ')
-
-        this.props.owner.translation = newVector
-        this.forceUpdate()
-      },
-      rotationChanged: function (oldValue, index, event) {
-        const oldVector = deserializeVector(oldValue)
-        oldVector[index] = event.currentTarget.value
-        const newVector = take(4, oldVector).join(' ')
-
-        this.props.owner.rotation = newVector
-        this.forceUpdate()
-      },
       render: function () {
         const attribute = this.props.attribute
-        const renderAttribute = this.state.renderAttribute
 
         if (attribute.name === 'render') {
           const checked = attribute.value === 'true'
           return (
             <span>
-              {attribute.name}: <input type='checkbox' checked={checked} onClick={this.clicked} />
+              {attribute.name}: <input type='checkbox' checked={checked} onChange={this.changeHandler} />
             </span>
           )
         } else if (contains(attribute.name, ['translation', 'rotation'])) {
           return (
             <span>
-              {attribute.name}: {(renderAttribute ? deserializeVector(attribute.value) : splitVector(attribute.value)).map((coordinate, index) => <input
-                type='text'
-                value={coordinate}
-                onFocus={this.focus}
-                onBlur={this.blur}
-                onChange={this.mapAttributeToHandler(attribute.name, attribute.value, index)}
-                style={ {width: '50px'} }
-              />)}
+              {attribute.name}: {splitVector(attribute.value).map((coordinate, index) =>
+                <TreeNodeAttributeTextbox
+                  attributeName={attribute.name}
+                  index={index}
+                  owner={this.props.owner}
+                  style={{width: '100px'}}
+                />
+              )}
             </span>
           )
         } else {
