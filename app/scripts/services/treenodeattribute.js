19b9210b9475754a9e12d95a0606a8b25c5a093c
Danny Arnold <despair.blue@gmail.com>
2015-05-19 16:53:51 +0200

    tree view attributes can be edited

--- app/scripts/services/treenodeattribute.js
+++ app/scripts/services/treenodeattribute.js
@@ -4,16 +4,22 @@ window.angular.module('scegratooApp')
   .service('TreeNodeAttribute', (React, R) => {
     const {
       contains,
+      curry,
       flatten,
       map,
       pipe,
-      split
+      split,
+      take
     } = R
 
-    const deserializeVector = pipe(
+    const splitVector = pipe(
       split(' '),
       map(split(',')),
-      flatten,
+      flatten
+    )
+
+    const deserializeVector = pipe(
+      splitVector,
       map(Number.parseFloat),
       map(n => n.toFixed(2))
     )
@@ -24,6 +30,20 @@ window.angular.module('scegratooApp')
         owner: React.PropTypes.object.isRequired,
         attribute: React.PropTypes.object.isRequired
       },
+      getInitialState: function () {
+        return {
+          renderAttribute: true
+        }
+      },
+      mapAttributeToHandler: function (name, value, index) {
+        if (name === 'translation') {
+          return curry(this.translationChanged)(value, index)
+        } else if (name === 'rotation') {
+          return curry(this.rotationChanged)(value, index)
+        } else {
+          throw new Error('Unknown attribute')
+        }
+      },
       clicked: function (event) {
         if (event.currentTarget.checked) {
           this.props.owner.render = true
@@ -31,8 +51,40 @@ window.angular.module('scegratooApp')
           this.props.owner.render = false
         }
       },
+      focus: function (translation, event) {
+        this.setState({
+          renderAttribute: false,
+          translation: this.props.owner.translation,
+          rotation: this.props.owner.rotation
+        })
+      },
+      blur: function (oldValue, index, event) {
+        this.setState({renderAttribute: true})
+      },
+      translationChanged: function (oldValue, index, event) {
+        const oldVector = deserializeVector(oldValue)
+        oldVector[index] = event.currentTarget.value
+        const newVector = take(3, oldVector).join(' ')
+
+        this.props.owner.translation = newVector
+        this.setState({
+          translation: newVector
+        })
+      },
+      rotationChanged: function (oldValue, index, event) {
+        const oldVector = deserializeVector(oldValue)
+        oldVector[index] = event.currentTarget.value
+        const newVector = take(4, oldVector).join(' ')
+
+        this.props.owner.rotation = newVector
+        this.setState({
+          rotation: newVector
+        })
+      },
       render: function () {
         const attribute = this.props.attribute
+
+        console.log('render: ', this.state)
 
         if (attribute.name === 'render') {
           const checked = attribute.value === 'true'
@@ -44,7 +96,14 @@ window.angular.module('scegratooApp')
         } else if (contains(attribute.name, ['translation', 'rotation'])) {
           return (
             <span>
-              {attribute.name}: {deserializeVector(attribute.value).map(coordinate => <input type='text' value={coordinate} style={ {width: '50px'} }/>)}
+              {attribute.name}: {(this.state.renderAttribute ? deserializeVector(attribute.value) : this.state[attribute.name].split(' ')).map((coordinate, index) => <input
+                type='text'
+                value={coordinate}
+                onFocus={this.focus}
+                onBlur={this.blur}
+                onChange={this.mapAttributeToHandler(attribute.name, attribute.value, index)}
+                style={ {width: '50px'} }
+              />)}
             </span>
           )
         } else {
