a8a95dc3231226f6641aec55cc8f001d3463e950
Danny Arnold <despair.blue@gmail.com>
2015-05-21 09:45:50 +0200

    make tree nodes collapsable

--- app/scripts/services/treenode.js
+++ app/scripts/services/treenode.js
@@ -11,6 +11,7 @@ window.angular.module('scegratooApp')
       filter,
       ifElse,
       map,
+      mergeAll,
       pipe,
       prop,
       toLower
@@ -29,9 +30,29 @@ window.angular.module('scegratooApp')
       isInline,
       always(undefined)
     )
+    const bulletStyle = {
+      width: '10px',
+      height: '10px',
+      borderRadius: '50%',
+      fontSize: '20px',
+      color: '#fff',
+      lineHeight: '100px',
+      textAlign: 'center',
+      background: '#000',
+      marginRight: '5px',
+      cursor: 'pointer'
+    }
+    const collapsedStyle = {
+      background: '#d9d9d9'
+    }
 
     const TreeNode = React.createClass({
       displayName: 'TreeNode',
+      getInitialState: function () {
+        return {
+          collapsed: false
+        }
+      },
       propTypes: {
         data: React.PropTypes.object.isRequired,
         runtime: React.PropTypes.object.isRequired
@@ -39,42 +60,74 @@ window.angular.module('scegratooApp')
       clicked: function (event) {
         this.props.runtime.showObject(this.props.data, 'xAxis')
       },
+      toggleVisibility: function (event) {
+        const element = event.currentTarget.parentNode.nextSibling
+
+        if (this.state.collapsed) {
+          element.style.display = ''
+          this.setState({
+            collapsed: false
+          })
+        } else {
+          element.style.display = 'none'
+          this.setState({
+            collapsed: true
+          })
+        }
+
+      },
       render: function () {
         const node = this.props.data
         const runtime = this.props.runtime
         const children = filter(complement(isGUI), node.children)
+        const collapsed = this.state.collapsed
 
         return (
-          <li ref='node'>
-            <a data-id={node.id} onClick={this.clicked}>
-              {`<${node.nodeName}>`}
-              <br/>
-            </a>
-            {map(
-              a => [
-                <TreeNodeAttribute attribute={a} owner={node} />,
-                <br/>
-              ],
-              filter(
-                pipe(
-                  prop('name'),
-                  toLower,
-                  contains(__, ['translation', 'rotation', 'diffusecolor', 'def', 'render', 'class'])
-                ),
-                node.attributes
-              )
-            )}
-            {unlessInline(node =>
-              <ul>
-                {map(child =>
-                  <TreeNode
-                    data={child}
-                    runtime={runtime}
-                  />
-                )(children)}
-              </ul>
-            )(node)}
-          </li>
+          <div>
+            <li ref='node' style={{listStyle:'none'}}>
+              <div
+                style={{display:'flex'}}
+              >
+                <div
+                  onClick={this.toggleVisibility}
+                  style={mergeAll([
+                    bulletStyle,
+                    collapsed && collapsedStyle
+                  ])}
+                />
+                <a data-id={node.id} onClick={this.clicked}>
+                  {`<${node.nodeName}>`}
+                  <br/>
+                </a>
+              </div>
+              <div style={{paddingLeft:'15px'}}>
+                {map(
+                  a => [
+                    <TreeNodeAttribute attribute={a} owner={node} />,
+                    <br/>
+                  ],
+                  filter(
+                    pipe(
+                      prop('name'),
+                      toLower,
+                      contains(__, ['translation', 'rotation', 'diffusecolor', 'def', 'render', 'class'])
+                    ),
+                    node.attributes
+                  )
+                )}
+                {unlessInline(node =>
+                  <ul>
+                    {map(child =>
+                      <TreeNode
+                        data={child}
+                        runtime={runtime}
+                      />
+                    )(children)}
+                  </ul>
+                )(node)}
+              </div>
+            </li>
+          </div>
         )
       }
     })
