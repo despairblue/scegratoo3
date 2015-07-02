198d2568fb65bfc258c0e46dbb1c1c7ab680c8a9
Danny Arnold <despair.blue@gmail.com>
2015-07-02 17:38:38 +0200

    remove useless tags

--- scegratoo/public/scripts/services/treeview.js
+++ scegratoo/public/scripts/services/treeview.js
@@ -14,14 +14,10 @@ window.angular.module('scegratooApp')
       render: function () {
         if (this.props.data.runtime) {
           return (
-            <div>
-              <ul>
-                <TreeNode
-                  data={this.props.data.querySelector('scene')}
-                  runtime={this.props.data.runtime}
-                />
-              </ul>
-            </div>
+            <TreeNode
+              data={this.props.data.querySelector('scene')}
+              runtime={this.props.data.runtime}
+            />
           )
         } else {
           return <div/>
